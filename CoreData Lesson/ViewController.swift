84ae4b26f3eac8f3bf6eb4e1e3c0c460d49747e6
Caio <caiovr13@gmail.com>
2021-03-30 21:33:13 -0300

    Trabalhando com filtros

--- CoreData Lesson/ViewController.swift
+++ CoreData Lesson/ViewController.swift
@@ -19,7 +19,35 @@ class ViewController: UIViewController {
     }
     
     func gettingData(context: NSManagedObjectContext) {
+        
+        // Criando requisição
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Product")
+        
+        // Criando ordenação
+        let ordenedRequestDescription = NSSortDescriptor(key: "prod_description", ascending: false)
+        let ordenedRequestPrice = NSSortDescriptor(key: "prod_price", ascending: true)
+        
+        // Aplicando ordenação à requisição
+        request.sortDescriptors = [ordenedRequestDescription, ordenedRequestPrice]
+        
+        // Criando filtro
+        //let predicate = NSPredicate(format: "prod_description == %@", "iMac 2018")
+        //let predicate = NSPredicate(format: "prod_description contains %@", "iMac")
+        //let predicate = NSPredicate(format: "prod_description contains [c] %@", "mac")
+        //let predicate = NSPredicate(format: "prod_description beginswith [c] %@", "mac")
+        //let predicate = NSPredicate(format: "prod_price >= %@", "1000")
+        
+        
+        // Aplicando filtro
+        //request.predicate = predicate
+        
+        
+        // Filtros compostos
+        let predicate1 = NSPredicate(format: "prod_description contains [c] %@", "mac")
+        let predicate2 = NSPredicate(format: "prod_description contains %@", "2018")
+        //let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate1, predicate2])
+        let compoundPredicate = NSCompoundPredicate(orPredicateWithSubpredicates: [predicate1, predicate2])
+        request.predicate = compoundPredicate
         
         do {
             let products =  try context.fetch(request)
@@ -51,7 +79,7 @@ class ViewController: UIViewController {
         
         
         // Configurando objeto
-        product.setValue("Cellphone", forKey: "prod_description")
+        product.setValue("iMac 2018", forKey: "prod_description")
         product.setValue(1199.99, forKey: "prod_price")
         product.setValue("pale blue sky", forKey: "prod_color")
         
